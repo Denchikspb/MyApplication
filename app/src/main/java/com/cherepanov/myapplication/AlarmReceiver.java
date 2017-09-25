96734e9dcf5df8647c135251ba61dff6fdce92ec
Denis Cherepanov <cherepanov.denis94@mail.ru>
2017-09-25 22:59:03 +0300

    add alarm clock functionality

---
+++ app/src/main/java/com/cherepanov/myapplication/AlarmReceiver.java
@@ -0,0 +1,27 @@
+package com.cherepanov.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Денис on 25.09.2017.
+ */
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //this will sound the alarm tone
+        //this will sound the alarm once, if you wish to
+        //raise alarm in loop continuously then use MediaPlayer and setLooping(true)
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+}
--- app/src/main/java/com/cherepanov/myapplication/fragment/AlarmClockFragment.java
+++ app/src/main/java/com/cherepanov/myapplication/fragment/AlarmClockFragment.java
@@ -2,16 +2,22 @@ package com.cherepanov.myapplication.fragment;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.cherepanov.myapplication.AlarmReceiver;
 import com.cherepanov.myapplication.R;
+
+import java.util.Calendar;
 
 /**
  * Created by Денис on 18.09.2017.
@@ -24,6 +30,8 @@ public class AlarmClockFragment extends Fragment {
     private TimePicker alarmTimePicker;
     private View view;
     private TextView textView;
+    private Button alarmOnBtn;
+    private Button alarmOffBtn;
 
     public static AlarmClockFragment getInstance() {
         Bundle args = new Bundle();
@@ -38,9 +46,53 @@ public class AlarmClockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.alarm_clock_fragment, container, false);
-        textView = (TextView) view.findViewById(R.id.alarm_clock_tv);
+        alarmTimePicker = (TimePicker) view.findViewById(R.id.timePicker);
+        alarmOnBtn = (Button) view.findViewById(R.id.alarm_on_btn);
+        alarmOffBtn = (Button) view.findViewById(R.id.alarm_off_btn);
+        textView = (TextView)view.findViewById(R.id.time_tv);
+        alarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getActivity(), AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        setupListener();
 
         return view;
+    }
+
+    private void setupListener() {
+        alarmOnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                powerAlarm();
+            }
+        });
+        alarmOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alarmManager.cancel(pendingIntent);
+                setTextTime("Будильник выключен");
+            }
+        });
+    }
+
+    private void powerAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+        calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+
+        int hour = alarmTimePicker.getCurrentHour();
+        int minute = alarmTimePicker.getCurrentMinute();
+
+        String minuteStr = String.valueOf(minute);
+;
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        if(minute < 10){
+            minuteStr = "0" + minuteStr;
+        }
+        textView.setText("Будильник поставлен на " + hour + ":" + minuteStr);
+    }
+
+    private void setTextTime(String s){
+        textView.setText(s);
     }
 
 }
