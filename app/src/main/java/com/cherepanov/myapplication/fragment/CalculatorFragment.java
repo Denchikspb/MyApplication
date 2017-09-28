fdc785f035596c93c72525b9747c19c8dc8ad7fc
Denis <cherepanov.denis94@mail.ru>
2017-09-28 14:57:55 +0300

    add functional calculator

--- app/src/main/java/com/cherepanov/myapplication/fragment/CalculatorFragment.java
+++ app/src/main/java/com/cherepanov/myapplication/fragment/CalculatorFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.cherepanov.myapplication.R;
 
@@ -15,9 +18,36 @@ import com.cherepanov.myapplication.R;
 
 public class CalculatorFragment extends Fragment {
 
+    private Button zeroBtn;
+    private Button oneBtn;
+    private Button twoBtn;
+    private Button threeBtn;
+    private Button fourBtn;
+    private Button fiveBtn;
+    private Button sixBtn;
+    private Button sevenBtn;
+    private Button eightBtn;
+    private Button nineBtn;
+    private Button dotBtn;
+    private Button addBtn;
+    private Button subBtn;
+    private Button divBtn;
+    private Button multiBtn;
+    private Button resultBtn;
+    private Button deleteBtn;
+    private Button clearBtn;
+    private EditText resultEdt;
+    private TextView resultInfoTV;
     private View view;
 
-    public static CalculatorFragment getInstance(){
+    private float valueOne;
+    private float valueTwo;
+    private boolean isAdd;
+    private boolean isSub;
+    private boolean isMulti;
+    private boolean isDiv;
+
+    public static CalculatorFragment getInstance() {
         Bundle args = new Bundle();
         CalculatorFragment fragment = new CalculatorFragment();
         fragment.setArguments(args);
@@ -28,9 +58,180 @@ public class CalculatorFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         view = inflater.inflate(R.layout.calculator_layout, container, false);
+        resultInfoTV = view.findViewById(R.id.info_tv);
+        resultEdt = view.findViewById(R.id.edit_text);
+        initButtons();
+        setupListener();
 
         return view;
+    }
+
+    private void setupListener() {
+        zeroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "0");
+            }
+        });
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "1");
+            }
+        });
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "2");
+            }
+        });
+        threeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "3");
+            }
+        });
+        fourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "4");
+            }
+        });
+        fiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "5");
+            }
+        });
+        sixBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "6");
+            }
+        });
+        sevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "7");
+            }
+        });
+        eightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "8");
+            }
+        });
+        nineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + "9");
+            }
+        });
+        dotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText(resultEdt.getText() + ".");
+            }
+        });
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultEdt.setText("");
+            }
+        });
+        initOperations();
+    }
+
+    private void initOperations() {
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (resultEdt == null) {
+                    resultEdt.setText("");
+                } else {
+                    valueOne = Float.parseFloat(resultEdt.getText() + "");
+                    isAdd = true;
+                    resultInfoTV.setText(resultEdt.getText() + "+");
+                    resultEdt.setText(null);
+                }
+            }
+        });
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                valueOne = Float.parseFloat(resultEdt.getText() + "");
+                isSub = true;
+                resultInfoTV.setText(resultEdt.getText() + "-");
+                resultEdt.setText(null);
+            }
+        });
+        multiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                valueOne = Float.parseFloat(resultEdt.getText() + "");
+                isMulti = true;
+                resultInfoTV.setText(resultEdt.getText() + "*");
+                resultEdt.setText(null);
+            }
+        });
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                valueOne = Float.parseFloat(resultEdt.getText() + "");
+                isDiv = true;
+                resultInfoTV.setText(resultEdt.getText() + "/");
+                resultEdt.setText(null);
+            }
+        });
+        resultBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                valueTwo = Float.parseFloat(resultEdt.getText() + "");
+                resultInfoTV.setText(resultEdt.getText() + "" + valueTwo);
+                float result = 0;
+                if (isAdd) {
+                    result = valueOne + valueTwo;
+                    resultEdt.setText(result + "");
+                    isAdd = false;
+                }
+                if (isSub) {
+                    result = valueOne - valueOne;
+                    resultEdt.setText(result + "");
+                    isSub = true;
+                }
+                if (isMulti) {
+                    result = valueOne * valueTwo;
+                    resultEdt.setText(result + "");
+                    isMulti = true;
+                }
+                if (isDiv) {
+                    result = valueOne / valueTwo;
+                    resultEdt.setText(result + "");
+                    isDiv = true;
+                }
+            }
+        });
+    }
+
+    private void initButtons() {
+        zeroBtn = view.findViewById(R.id.zero_btn);
+        oneBtn = view.findViewById(R.id.one_btn);
+        twoBtn = view.findViewById(R.id.two_btn);
+        threeBtn = view.findViewById(R.id.three_btn);
+        fourBtn = view.findViewById(R.id.four_btn);
+        fiveBtn = view.findViewById(R.id.five_btn);
+        sixBtn = view.findViewById(R.id.six_btn);
+        sevenBtn = view.findViewById(R.id.seven_btn);
+        eightBtn = view.findViewById(R.id.eight_btn);
+        nineBtn = view.findViewById(R.id.nine_btn);
+        dotBtn = view.findViewById(R.id.dot_btn);
+        addBtn = view.findViewById(R.id.add_btn);
+        subBtn = view.findViewById(R.id.sub_btn);
+        divBtn = view.findViewById(R.id.div_btn);
+        multiBtn = view.findViewById(R.id.multi_btn);
+        resultBtn = view.findViewById(R.id.result_btn);
+        deleteBtn = view.findViewById(R.id.delete_btn);
+        clearBtn = view.findViewById(R.id.clear_btn);
     }
 }
